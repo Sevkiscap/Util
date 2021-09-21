82f9550326a8789d72ece906a70455cd34491a18
Sevkiscap <63850097+Sevkiscap@users.noreply.github.com>
2021-09-21 16:42:26 -0700

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,6 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <string>
 #include <iostream>
 int intArray[] = { 1,2,3,4,5,6 };
 float floatArray[] = { 1.0,2.0,3.0,4.0,5.0,6.0 };
@@ -13,10 +13,20 @@ int main()
     for (int x = sizeof(intArray) / 2; x < sizeof(intArray); x+=4) {
         intArray[x / 4] = NULL;
     }
-    for (int x = sizeof(intArray) / 2; x < sizeof(intArray); x += 4) {
-        intArray[x / 4] = NULL;
+    for (int x = sizeof(floatArray) / 2; x < sizeof(floatArray); x += 4) {
+        floatArray[x / 4] = NULL;
     }
-    
+    for (int x = sizeof(doubleArray) / 2; x < sizeof(doubleArray); x += 4) {
+        doubleArray[x / 4] = NULL;
+    }
+    for (int x = sizeof(stringArray) / 2; x < sizeof(stringArray); x+=4) {
+        stringArray[x / 4] = "";
+    }
+    size_t length = sizeof(intArray) / sizeof(intArray[0]);
+
+    for (size_t x = 0; x > length; x++) {
+        std::cout << intArray[x];
+    }
 }
 
 
